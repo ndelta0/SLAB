6c768ca593bffadc35003cf01dd350603756e05f
March3wQa <piotrek.marchewka123@gmail.com>
2018-11-29 20:11:09 +0100

    Uploading existing files

---
+++ discord_bbone.py
@@ -0,0 +1,141 @@
+import spotify_api as sapi
+import discord
+from discord.compat import create_task
+import asyncio
+
+# Variables
+PREF = '!!'
+boundChannel = '516168648373698563'
+DISCORDTOKEN = 'NTE2MTY5MTUxODQ1MzY3ODA5.DtvvnQ.3x4uiFEM5OlXrXeqPuMDBIfOtKY'
+# <----->
+
+client = discord.Client()
+
+async def statusChange():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        while 1:
+            await client.change_presence(game = discord.Game(name='Spotiscord v1'))
+            await asyncio.sleep(15)
+            helpStr = 'Type %shelp for help!' % PREF
+            await client.change_presence(game = discord.Game(name=helpStr))
+            await asyncio.sleep(15)
+
+@client.event
+async def on_message(message):
+    global PREF
+    global boundChannel
+    if message.channel.id == boundChannel:
+        if message.author == client.user:
+            return
+        if message.content.lower().startswith('%shello' % PREF):
+            msg = 'Hello {0.author.mention}'.format(message)
+            await client.send_message(message.channel, msg)
+        if message.content.lower().startswith('%ssearch' % PREF):
+            msg = message.content.lower()
+            msgList = msg.split()
+            msgList.pop(0)
+            if msgList == []:
+                await client.send_message(message.channel, ':x:** Proper use:** `%ssearch <query>`' % PREF)
+                return
+            msg = ' '.join(msgList)
+            # await client.send_message(message.channel, msg)
+            response = sapi.searchSong(msg)
+            if response == 'Something went wrong. Try again.':
+                await client.send_message(message.channel, response)
+            elif response == 'No results.':
+                await client.send_message(message.channel, response)
+            elif response != (('Something went wrong. Try again.') or ('No results.')):
+                await client.send_message(message.channel, 'Is this the song you are looking for?')
+                await client.send_message(message.channel, response[0])
+                await client.send_message(message.channel, 'If yes, type \'**!!yes**\', and if not type \'**!!no**\' and specify your search')
+                def agreement(ans):
+                    if ans.content.startswith('%syes' % PREF):
+                        return('yes')
+                    else:
+                        return('no')
+                ans = await client.wait_for_message(author=message.author, check=agreement, timeout=30)
+                if ans.content == '{}yes'.format(PREF):
+                    await client.send_message(message.channel, 'Adding to playlist')
+                    response = sapi.addToPlaylist(response[1])
+                    await client.send_message(message.channel, response)
+                else:
+                    await client.send_message(message.channel, 'Cancelled')
+        if message.content.lower().startswith('%screate' % PREF):
+            if (message.author.roles[len(message.author.roles)-1].permissions.administrator or message.author.roles[len(message.author.roles)-1].permissions.manage_channels or message.author.roles[len(message.author.roles)-1].permissions.manage_server) == True:
+                msg = message.content.lower()
+                msgList = msg.split()
+                msgList.pop(0)
+                if msgList == []:
+                    await client.send_message(message.channel, ':x:** Proper use:** `%screate <playlist name>`' % PREF)
+                    return
+                msg = ' '.join(msgList)
+                response = sapi.createPlaylist(msg)
+                if response[0] == 'Error creating playlist.':
+                    await client.send_message(message.channel, response[0])
+                elif response[0] == 'Playlist already exists.':
+                    await client.send_message(message.channel, response[0])
+                elif response[0] != 'Error creating playlist.' or 'Playlist already exists.':
+                    await client.send_message(message.channel, response[0])
+                    await client.send_message(message.channel, response[1]['playlist_url'])
+            else:
+                await client.send_message(message.channel, ':x:***You are not allowed to execute that command!***')
+        if message.content.lower().startswith('%sdelete' % PREF):
+            if (message.author.roles[len(message.author.roles)-1].permissions.administrator or message.author.roles[len(message.author.roles)-1].permissions.manage_channels or message.author.roles[len(message.author.roles)-1].permissions.manage_server) == True:
+                response = sapi.removePlaylist()
+                if response == 'Deleted successfully.':
+                    await client.send_message(message.channel, response)
+                else:
+                    await client.send_message(message.channel, response)
+            else:
+                await client.send_message(message.channel, ':x:***You are not allowed to execute that command!***')
+        if message.content.lower().startswith('%splaylist' % PREF):
+            response = sapi.getPlaylist()
+            await client.send_message(message.channel, response)
+        if message.content.lower().startswith('%sprefix' % PREF):
+            if (message.author.roles[len(message.author.roles)-1].permissions.administrator or message.author.roles[len(message.author.roles)-1].permissions.manage_channels or message.author.roles[len(message.author.roles)-1].permissions.manage_server) == True:
+                msg = message.content.lower()
+                msgList = msg.split()
+                msgList.pop(0)
+                if msgList == []:
+                    await client.send_message(message.channel, ':x:** Proper use:** `%sprefix <prefix>`' % PREF)
+                    return
+                msg = ' '.join(msgList)
+                PREF = msg
+                await client.send_message(message.channel, 'Changed prefix to `%s`' % PREF)
+                return PREF
+            else:
+                await client.send_message(message.channel, ':x:***You are not allowed to execute that command!***')
+        if message.content.lower().startswith('%shelp' % PREF):
+            await client.send_message(message.channel, 'Available commands:\n```%ssearch <query> - searches for a song to add it to playlist\n%splaylist - shows the playlist\n%shelp - shows this help```\nAdditional commands for admins:\n```%screate <name> - creates playlist with name <name>\n%sdelete - deletes playlist\n%sprefix <new prefix> - sets the prefix\n%sbind - binds bot to specified channel (where it accepts commands```\n**NOTE:** Bot only allows for one playlist for now!' % (PREF, PREF, PREF, PREF, PREF, PREF, PREF))
+
+
+        if message.content.lower().startswith('%sverify' % PREF):
+            pass
+
+
+
+        else:
+            await client.send_message(message.channel, 'Unknown command. Use %shelp for list of available commands.' % PREF)
+    elif message.content.lower().startswith('%sbind' % PREF):
+        if (message.author.roles[len(message.author.roles)-1].permissions.administrator or message.author.roles[len(message.author.roles)-1].permissions.manage_channels or message.author.roles[len(message.author.roles)-1].permissions.manage_server) == True:
+            if boundChannel == message.channel.id:
+                await client.send_message(message.channel, 'Already bound')
+            else:
+                boundChannel = message.channel.id
+                await client.send_message(message.channel, '***Bound to this channel.***')
+                return boundChannel
+        else:
+            await client.send_message(message.channel, ':x:***You are not allowed to execute that command!***')
+    else:
+        await client.send_message(message.channel, 'Invalid channel')
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.loop.create_task(statusChange())
+client.run(DISCORDTOKEN)
---
+++ spotify_api.py
@@ -0,0 +1,169 @@
+## Imports
+import requests as rq
+import json
+import webbrowser as wb
+import base64
+
+with open('bot\\tokens.json') as f:
+	tokensStr = json.load(f)
+	tokens = json.loads(tokensStr)
+
+with open('bot\\playlists.json') as f:
+	playlistsStr = json.load(f)
+	playlists = json.loads(playlistsStr)
+
+## Variables
+playlistURL = playlists["playlist_url"]
+playlistID = playlists["playlist_id"]
+accessToken = tokens["access_token"]
+refreshToken = tokens["refresh_token"]
+clientID = 'd3df69ad53ad4fe0afe621a68a2e852b'
+clientSecret = '9d998fca5b7444aa9ebf43c590e5d5c6'
+header = {'Authorization': 'Bearer '+ accessToken}
+
+## Functions
+def tokenSwap():
+	global clientID
+	global clientSecret
+
+	wb.open('https://accounts.spotify.com/authorize?client_id=d3df69ad53ad4fe0afe621a68a2e852b&response_type=code&redirect_uri=http://localhost/&scope=playlist-modify-public', new=2)
+	apiCode = input('Code >> ')
+
+	authKey = clientID + ':' + clientSecret
+	authKeyBytes = str.encode(authKey)
+	authKeyBytes = base64.b64encode(authKeyBytes)
+	authKey = bytes.decode(authKeyBytes)
+	authHeader = {'Authorization': 'Basic '+ authKey}
+	dataBody = {'grant_type': 'authorization_code', 'code': apiCode, 'redirect_uri': 'http://localhost/'}
+
+	resp = rq.post(url = 'https://accounts.spotify.com/api/token', data = dataBody, headers = authHeader)
+	respJson = resp.json()
+
+	if resp.status_code == 200:
+		global accessToken
+		global refreshToken
+		global header
+		accessToken = respJson['access_token']
+		refreshToken = respJson['refresh_token']
+		header['Authorization'] = 'Bearer '+ accessToken
+		tokens = {'access_token': accessToken, 'refresh_token': refreshToken}
+		tokensJson = json.dumps(tokens)
+		with open('bot\\tokens.json', 'w') as f:
+			json.dump(tokensJson, f)
+		return accessToken, refreshToken, header
+	else:
+		print(respJson['error'] + ' >> ' + respJson['error_description'])
+		return('Something went terribly wrong')
+	
+def tokenRefresh():
+	global accessToken
+	global refreshToken
+
+	dataBody = {'grant_type': 'refresh_token', 'refresh_token': refreshToken}
+	authKey = clientID + ':' + clientSecret
+	authKeyBytes = str.encode(authKey)
+	authKeyBytes = base64.b64encode(authKeyBytes)
+	authKey = bytes.decode(authKeyBytes)
+	authHeader = {'Authorization': 'Basic '+ authKey}
+
+	resp = rq.post(url = 'https://accounts.spotify.com/api/token', data = dataBody, headers = authHeader)
+	respJson = resp.json()
+
+	if resp.status_code == 200:
+		global header
+		accessToken = respJson['access_token']
+		header['Authorization'] = 'Bearer '+ accessToken
+		tokens = {'access_token': accessToken, 'refresh_token': refreshToken}
+		tokensJson = json.dumps(tokens)
+		with open('bot\\tokens.json', 'w') as f:
+			json.dump(tokensJson, f)
+		return accessToken, header
+	else:
+		print(respJson['error'] + ' >> ' + respJson['error_description'])
+		tokenSwap()
+
+def searchSong(q, market = 'PL'):
+	query = q.replace(' ', '+')
+	params = {'q': query, 'type': 'track', 'limit': 1, 'offset': 0, 'market': market}
+	resp = rq.get(url = 'https://api.spotify.com/v1/search', params = params, headers = header)
+	respJson = resp.json()
+	with open('response.json', 'w') as f:
+		json.dump(respJson, f)
+	if resp.status_code == 200:
+		if respJson['tracks']['items'] == []:
+			searchSong(q, 'US')
+		else:
+			trackURL = respJson['tracks']['items'][0]['external_urls']['spotify']
+			trackURI = respJson['tracks']['items'][0]['uri']
+			return(trackURL, trackURI)
+	else:
+		if resp.status_code == 401:
+			print(str(respJson['error']['status']) + ' >> ' + respJson['error']['message'])
+			tokenRefresh()
+			return('Something went wrong. Try again.')
+		else:
+			print(str(respJson['error']['status']) + ' >> ' + respJson['error']['message'])
+			return('Something went wrong. Try again.')
+
+def createPlaylist(name):
+    global playlistID
+    if playlistID == 'none':
+        dataPost = '{\"name\": \"%s\"}' % name
+        customHeader = header
+        headerAdditional = {'Content-Type': 'application/json'}
+        customHeader.update(headerAdditional)
+
+        resp = rq.post(url = 'https://api.spotify.com/v1/users/11172683931/playlists', data = dataPost, headers = customHeader)
+        respJson = resp.json()
+
+        if resp.status_code == 200 or resp.status_code == 201:
+            global playlistURL
+            playlistID = respJson['id']
+            playlistURL = respJson['external_urls']['spotify']
+            playlists = {'playlist_url': playlistURL, 'playlist_id': playlistID}
+            playlistsJson = json.dumps(playlists)
+            with open('bot\\playlists.json', 'w') as f:
+                json.dump(playlistsJson, f)
+            return ['Created playlist: **%s**' % name, playlists]
+        else:
+            print(str(respJson['error']['status']) + ' >> ' + respJson['error']['message'])
+            return ['Error creating playlist.']
+    else:
+        return ['Playlist already exists.']
+
+def removePlaylist():
+    global playlistID
+    global playlistURL
+    url = 'https://api.spotify.com/v1/playlists/%s/followers' % playlistID
+    resp = rq.delete(url = url, headers = header)
+
+    if resp.status_code == 200:
+        playlists = {'playlist_url': 'none', 'playlist_id': 'none'}
+        playlistsJson = json.dumps(playlists)
+        with open('bot\\playlists.json', 'w') as f:
+            json.dump(playlistsJson, f)
+        return('Deleted successfully.')
+    else:
+        respJson = resp.json()
+        print(str(respJson['error']['status']) + ' >> ' + respJson['error']['message'])
+        return('Unable to delete playlist.')
+
+def addToPlaylist(uri):
+    global playlistID
+    url = 'https://api.spotify.com/v1/playlists/%s/tracks' % playlistID
+    params = {'uris': uri}
+    resp = rq.post(url = url, params = params, headers = header)
+
+    if resp.status_code == 201:
+        return('Added successfully.')
+    else:
+        respJson = resp.json()
+        print(str(respJson['error']['status']) + ' >> ' + respJson['error']['message'])
+        return('Unable to add to playlist.')
+
+def getPlaylist():
+    global playlistURL
+    return playlistURL
+
+def verifyPremium():
+    
